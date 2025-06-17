405a854504fa7feeba51d244e320ac7c8124cc78
irrvlo <51727632+irrvlo@users.noreply.github.com>
2025-06-17 15:58:21 -0500

    Juice Client End of Life

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 require("v8-compile-cache");
-const { app } = require("electron");
-const { initSplash } = require("./windows/splash");
-const { initResourceSwapper } = require("./addons/swapper");
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
 
 app.on("ready", () => {
-  initSplash();
-  initResourceSwapper();
+  mainWindow = new BrowserWindow({
+    icon: "src/assets/img/icon.png",
+    width: 1280,
+    height: 720,
+    backgroundColor: "#141414",
+  });
+  mainWindow.loadFile("src/index.html");
+  mainWindow.show();
 });
 
 app.on("window-all-closed", () => app.quit());
